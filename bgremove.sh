457964a107ce47e8b57e4ae3918f10bf94fdcbed
Aymeric Wilke <aywilke@gmail.com>
2023-03-31 15:09:12 +0200

    Remove query parameters in the image URL

--- bgremove.sh
+++ bgremove.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID && aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY && aws configure set default.region $AWS_DEFAULT_REGION
-filename=$(basename "$1")
+url=$(echo "$1" | cut -d'?' -f1)
+filename=$(basename "$url")
 wget -O $filename $1
 backgroundremover -i $filename -o output.png
 aws s3 cp output.png s3://$2
